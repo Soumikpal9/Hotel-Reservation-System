8acbfde85339a393d4bbef0dde1324a6bb381e18
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-15 12:42:33 +0530

    [SP]Added: Welcome Message

---
+++ src/com/cg/HotelDetails.java
@@ -0,0 +1,11 @@
+package com.cg;
+
+public class HotelDetails {
+	public String name;
+	public int ratesRegularCustomer;
+	
+	public HotelDetails(String name, int ratesRegularCustomer) {
+		this.name = name;
+		this.ratesRegularCustomer = ratesRegularCustomer;
+	}
+}
